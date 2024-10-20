da3c17449d806374c7615c520f580b0b1e0b0010
elitf <elitfuturae@gmail.com>
2024-10-20 13:55:54 +0300

    Modified location model.

--- crowdfunding_platform/user_facilities/models/Location.py
+++ crowdfunding_platform/user_facilities/models/Location.py
@@ -4,8 +4,12 @@ class Location(models.Model):
     country = models.CharField(max_length=100)
     state = models.CharField(max_length=100, blank=True, null=True)
     city = models.CharField(max_length=100)
-    postal_code = models.CharField(unique=True, max_length=20)
+    postal_code = models.CharField(max_length=20, unique=True)
 
     class Meta:
         managed = False
         db_table = 'location'
+        indexes = [
+            models.Index(fields=['country']),
+            models.Index(fields=['city']),
+        ]

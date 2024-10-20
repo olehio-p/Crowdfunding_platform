84170de5c6771e2fa8c317709d61271cc28c7c90
elitf <elitfuturae@gmail.com>
2024-10-20 14:07:15 +0300

    Added reward claim model to project.

---
+++ crowdfunding_platform/reward_facilities/models/Reward_Claim.py
@@ -0,0 +1,21 @@
+from django.db import models
+
+from crowdfunding_platform.reward_facilities.models.Reward import Reward
+from crowdfunding_platform.user_facilities.models.User import CustomUser
+
+
+class RewardClaim(models.Model):
+    reward = models.ForeignKey(Reward, on_delete=models.CASCADE, related_name='reward_claims')
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name='reward_claims')
+    claim_date = models.DateTimeField(auto_now_add=True)
+    is_fulfilled = models.BooleanField(default=False)
+
+    class Meta:
+        managed = False
+        db_table = 'reward_claim'
+        unique_together = (('reward', 'user'),)
+        indexes = [
+            models.Index(fields=['reward']),
+            models.Index(fields=['user']),
+            models.Index(fields=['is_fulfilled', 'claim_date']),
+        ]

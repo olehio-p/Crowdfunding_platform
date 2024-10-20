8bd0bebb0abf1a3cdf488f65988de12585700d09
elitf <elitfuturae@gmail.com>
2024-10-20 13:42:52 +0300

    Added update model to project.

---
+++ crowdfunding_platform/project_facilities/models/Update.py
@@ -0,0 +1,19 @@
+from django.db import models
+from django.utils import timezone
+
+from crowdfunding_platform.project_facilities.models.Project import Project
+
+
+class Update(models.Model):
+    project = models.ForeignKey(Project, on_delete=models.CASCADE, related_name='updates')
+    title = models.CharField(max_length=255)
+    content = models.TextField(null=True, blank=True)
+    date = models.DateTimeField(default=timezone.now)
+
+    class Meta:
+        managed = False
+        db_table = 'update'
+        indexes = [
+            models.Index(fields=['project']),
+            models.Index(fields=['date']),
+        ]

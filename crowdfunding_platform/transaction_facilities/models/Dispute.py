36c246de2b25a4ac732e21388d905131e2abe6d7
elitf <elitfuturae@gmail.com>
2024-10-20 13:45:02 +0300

    Added dispute model to project.

---
+++ crowdfunding_platform/transaction_facilities/models/Dispute.py
@@ -0,0 +1,28 @@
+from django.db import models
+from django.utils import timezone
+
+from crowdfunding_platform.transaction_facilities.models.Transaction import Transaction
+from crowdfunding_platform.user_facilities.models.User import CustomUser
+
+
+class Dispute(models.Model):
+    transaction = models.ForeignKey(Transaction, on_delete=models.CASCADE, related_name='disputes')
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name='disputes')
+    reason = models.TextField()
+    status = models.CharField(max_length=20, choices=[
+        ('open', 'Open'),
+        ('closed', 'Closed'),
+        ('in review', 'In Review')
+    ], default='open')
+    created_at = models.DateTimeField(default=timezone.now)
+    resolution_date = models.DateTimeField(null=True, blank=True)
+
+    class Meta:
+        managed = False
+        db_table = 'dispute'
+        indexes = [
+            models.Index(fields=['transaction']),
+            models.Index(fields=['user']),
+            models.Index(fields=['status', 'created_at']),
+            models.Index(fields=['created_at']),
+        ]

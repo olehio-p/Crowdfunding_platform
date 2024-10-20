e5dc8a374d9147c5274f961dad52a15d891370e3
elitf <elitfuturae@gmail.com>
2024-10-20 15:36:59 +0300

    Updated configs and structure.

--- crowdfunding_platform/analytics_facilities/apps.py
+++ crowdfunding_platform/analytics_facilities/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class AnatyticsFacilitiesConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
-    name = "anatytics_facilities"
+    name = "analytics_facilities"
--- crowdfunding_platform/crowdfunding_platform/settings.py
+++ crowdfunding_platform/crowdfunding_platform/settings.py
@@ -37,6 +37,12 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "user_facilities",
+    "project_facilities",
+    "transaction_facilities",
+    "reward_facilities",
+    "notifications_facilities",
+    "analytics_facilities"
 ]
 
 MIDDLEWARE = [

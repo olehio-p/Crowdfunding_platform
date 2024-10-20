8fe7556dcc5b979701c06f4f061be70cb00163db
elitf <elitfuturae@gmail.com>
2024-10-20 13:26:32 +0300

    Added language model to users app.

---
+++ crowdfunding_platform/user_facilities/models/Language.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+class Language(models.Model):
+    code = models.CharField(unique=True, max_length=10)
+    name = models.CharField(max_length=100)
+
+    class Meta:
+        managed = False
+        db_table = 'language'
+

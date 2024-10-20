55f493178be20c6e182a18be5d293de301949640
elitf <elitfuturae@gmail.com>
2024-10-20 14:15:03 +0300

    Added new app for managing notifications facilities to project.

---
+++ crowdfunding_platform/notifications_facilities/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class NotificationsFacilitiesConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "notifications_facilities"

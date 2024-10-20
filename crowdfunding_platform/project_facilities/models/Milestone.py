699e66b20754a8f8537fd323a92d3c023ea8d79c
elitf <elitfuturae@gmail.com>
2024-10-20 13:54:50 +0300

    Modified milestone model.

--- crowdfunding_platform/project_facilities/models/Milestone.py
+++ crowdfunding_platform/project_facilities/models/Milestone.py
@@ -1,11 +1,14 @@
 from django.db import models
 
+from crowdfunding_platform.project_facilities.models.Project import Project
+
+
 class Milestone(models.Model):
-    project = models.ForeignKey('Project', models.DO_NOTHING)
+    project = models.ForeignKey(Project, on_delete=models.CASCADE, related_name='milestones')
     title = models.CharField(max_length=255)
-    description = models.CharField(max_length=1000, blank=True, null=True)
+    description = models.TextField(max_length=1000, null=True, blank=True)
     goal = models.DecimalField(max_digits=10, decimal_places=2)
-    completion_date = models.DateField(blank=True, null=True)
+    completion_date = models.DateField(null=True, blank=True)
 
     class Meta:
         managed = False

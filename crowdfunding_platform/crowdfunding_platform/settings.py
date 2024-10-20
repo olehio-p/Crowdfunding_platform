9d10804d9ac335088b57be7065de360df6562601
elitf <elitfuturae@gmail.com>
2024-10-20 12:52:56 +0300

    Added base structure for project app facilities.

--- crowdfunding_platform/crowdfunding_platform/settings.py
+++ crowdfunding_platform/crowdfunding_platform/settings.py
@@ -74,9 +74,13 @@ WSGI_APPLICATION = "crowdfunding_platform.wsgi.application"
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'crowdfunding_platform',
+        'USER': 'root',
+        'PASSWORD': 'vsepBmx6',
+        'HOST':'localhost',
+        'PORT':'3306',
     }
 }
 
